5c94817544a18d48dd31b7346ed08eee0062fde0
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2023-05-08 20:43:58 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// typed js library
+let typed = new typed(".typing", {
+  strings: ["FrontEnd Developer", "applications Mobile"],
+  typeSpeed: 100,
+  BackSpeed: 200,
+  backDelay: 1000,
+  loop: true,
+});
+// ========================
+let type = new typed(".multiple-text", {
+  strings: ["Abdallh Elzayat", "frontEnd Developer"],
+  typeSpeed: 100,
+  BackSpeed: 200,
+  backDelay: 1000,
+  loop: true,
+});
